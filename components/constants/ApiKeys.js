09f9b731db47c50c1bae560f6821d0453078b623
Stephane Darcy SIMO MBA <stephanedarcy.simomba@epitech.eu>
2019-01-05 11:40:45 +0100

    Add ApiKey constants

---
+++ components/constants/ApiKeys.js
@@ -0,0 +1,11 @@
+
+export default {
+    FirebaseConfig: {
+        apiKey: "AIzaSyAEhi4ehf1tqIDFIn2SB0dTmhyY51Pjaqg",
+        authDomain: "insta6credit.firebaseapp.com",
+        databaseURL: "https://insta6credit.firebaseio.com",
+        projectId: "insta6credit",
+        storageBucket: "insta6credit.appspot.com",
+        messagingSenderId: "881293053273"
+    }
+}
